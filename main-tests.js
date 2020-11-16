8e0fad350fc8c44b041b29d8b0cceca5357fee81
Rosano Coutinho <pub@xeari.com>
2020-11-16 11:43:13 -0300

    Spec OLSKGuardThrow

---
+++ main-tests.js
@@ -0,0 +1,60 @@
+const { throws, deepEqual } = require('assert');
+
+const mod = require('./main.js');
+
+describe('OLSKGuardThrow', function test_OLSKGuardThrow() {
+
+	it('throws if param1 not object', function() {
+		throws(function() {
+			mod.OLSKGuardThrow(null, [Math.random().toString()]);
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('throws if param2 not array', function() {
+		throws(function() {
+			mod.OLSKGuardThrow({}, null);
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('throws if param2 not filled', function() {
+		throws(function() {
+			mod.OLSKGuardThrow({}, []);
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('throws if param2 element not string', function() {
+		throws(function() {
+			mod.OLSKGuardThrow({}, [null]);
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('throws if param2 element not filled', function() {
+		throws(function() {
+			mod.OLSKGuardThrow({}, [' ']);
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('throws if param2 elements not keys of param1', function() {
+		const item = Date.now().toString();
+		throws(function() {
+			mod.OLSKGuardThrow({}, [item]);
+		}, new RegExp(`${ item } not defined`));
+	});
+
+	it('throws if param1[param2 element] not filled', function() {
+		const item = Date.now().toString();
+		throws(function() {
+			mod.OLSKGuardThrow({
+				[item]: ' ',
+			}, [item]);
+		}, new RegExp(`${ item } blank`));
+	});
+
+	it('returns undefined', function() {
+		const item = Date.now().toString();
+		deepEqual(mod.OLSKGuardThrow({
+			[item]: Math.random().toString(),
+		}, [item]), undefined);
+	});
+
+});
---
+++ main.js
@@ -0,0 +1,37 @@
+const mod = {
+
+	OLSKGuardThrow (param1, param2) {
+		if (typeof param1 !== 'object' || param1 === null) {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		if (!Array.isArray(param2)) {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		if (!param2.length) {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		param2.forEach(function (e) {
+			if (typeof e !== 'string') {
+				throw new Error('OLSKErrorInputNotValid');
+			}
+
+			if (!e.trim()) {
+				throw new Error('OLSKErrorInputNotValid');
+			}
+
+			if (!param1[e]) {
+				throw new Error(`${ e} not defined`);
+			}
+
+			if (!param1[e].trim()) {
+				throw new Error(`${ e} blank`);
+			}
+		});
+	},
+
+};
+
+Object.assign(exports, mod);
